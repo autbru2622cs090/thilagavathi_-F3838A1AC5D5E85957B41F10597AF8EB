5ffa17c83f932647be14d85bf275ee5062c8853a
autbru2622cs090 <144418282+autbru2622cs090@users.noreply.github.com>
2023-10-28 13:54:08 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,35 +1,7 @@
-#
-class BankAccount:
-    def __init__(self, account_number, account_holder_name, initial_balance=0):
-        self.__account_number = account_number
-        self.__account_holder_name = account_holder_name
-        self.__account_balance = initial_balance
-
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            return f"Deposited ₹{amount}. New balance: ₹{self.__account_balance}"
-        else:
-            return "Invalid deposit amount. Please enter a positive amount."
-
-    def withdraw(self, amount):
-        if 0 < amount <= self.__account_balance:
-            self.__account_balance -= amount
-            return f"Withdrew ₹{amount}. New balance: ₹{self.__account_balance}"
-        elif amount > self.__account_balance:
-            return "Insufficient funds."
-        else:
-            return "Invalid withdrawal amount. Please enter a positive amount."
-
-    def display_balance(self):
-        return f"Account balance for {self.__account_holder_name}: ₹{self.__account_balance}"
-
-# Testing the BankAccount class
-if __name__ == "__main__":
-    account1 = BankAccount("123456789", "Ganeshkumar", 1000)
-
-    print(account1.display_balance())
-    print(account1.deposit(500))
-    print(account1.withdraw(200))
-    print(account1.withdraw(1500))
-    print(account1.display_balance())
+#Online Python computer interpreters to Fun Python online.
+#write Python 3 code in this online editor and run it.
+year = 2020
+if(year%4 ==0):
+   print("leap year")
+else:
+    print("Not leap year")
